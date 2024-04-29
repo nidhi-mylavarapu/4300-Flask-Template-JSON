ced460549db529625ace5e5831f8012fceb318b6
nathanpalamuttam <npalamuttam04@gmail.com>
2024-04-29 10:26:30 -0400

    get rid of space at the last element of liked genres and add one image to json

--- backend/app.py
+++ backend/app.py
@@ -59,7 +59,6 @@ def filter_movies_by_genre(genre):
         return False
     items_list = genre.split(",")
     matching_rows = []
-    maxNum = 0
     for index, row in movies_df.iterrows():
         count = 0
         dicOfGenres = ast.literal_eval(row['genres'])
